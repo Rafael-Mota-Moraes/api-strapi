9ee5753990ff3ae66a60dce4045c417bbffc9cc0
Rafael-Mota-Moraes <rafaelmota622@gmail.com>
2022-09-30 12:37:15 -0300

    enviando configs para o .env

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', 'babar.db.elephantsql.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'ftahaauf'),
-      user: env('DATABASE_USERNAME', 'ftahaauf'),
-      password: env('DATABASE_PASSWORD', 'YbisOh9CX2IaCyTRsF0qpYaVNSsCO4vj'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env("DATABASE_HOST", ""),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", ""),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+      ssl: env.bool("DATABASE_SSL", true),
     },
   },
 });
